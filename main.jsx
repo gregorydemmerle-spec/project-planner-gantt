02dd36945813b12e40dce4a4c4ef020a70dfaa46
gregdemmerle <gregory.demmerle@gmail.com>
2026-03-29 17:02:22 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import React from "react";\
+import ReactDOM from "react-dom/client";\
+import App from "./App.jsx";\
+import "./index.css";\
+\
+ReactDOM.createRoot(document.getElementById("root")).render(\
+\'a0 <React.StrictMode>\
+\'a0\'a0\'a0 <App />\
+\'a0 </React.StrictMode>\
+);\
+}
